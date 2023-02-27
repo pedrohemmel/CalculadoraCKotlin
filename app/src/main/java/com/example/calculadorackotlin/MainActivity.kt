360bed4e0fe67ab00590430d4d31faaf1b6c7dde
pedrohemmel <phenrique_h2016@hotmail.com>
2023-02-27 20:30:50 -0300

    Inserindo objetos para calculo

--- app/src/main/java/com/example/calculadorackotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculadorackotlin/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.calculadorackotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var txtView: TextView = findViewById(R.id.meuTxtView)
+        val txtFieldValor1: EditText = findViewById(R.id.txtFieldValor1)
+        val txtFieldValor2: EditText = findViewById(R.id.txtFieldValor2)
+        val btnSomar: Button = findViewById(R.id.btnSomar)
+
+        btnSomar.setOnClickListener {
+            txtView.setText("${txtFieldValor1.text.toString().toInt() + txtFieldValor2.text.toString().toInt()}")
+        }
     }
 }
