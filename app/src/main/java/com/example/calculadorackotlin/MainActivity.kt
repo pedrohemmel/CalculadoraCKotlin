f53795fcd0af1cf5d41a99bbe15e463d00635978
pedrohemmel <phenrique_h2016@hotmail.com>
2023-02-27 21:31:51 -0300

    Adicionando funções de multiplicar, dividir e subtrair

--- app/src/main/java/com/example/calculadorackotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculadorackotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadorackotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,9 +18,23 @@ class MainActivity : AppCompatActivity() {
         val txtFieldValor1: EditText = findViewById(R.id.txtFieldValor1)
         val txtFieldValor2: EditText = findViewById(R.id.txtFieldValor2)
         val btnSomar: Button = findViewById(R.id.btnSomar)
+        val btnMultiplicar: Button = findViewById(R.id.btnMultiplicar)
+        val btnDividir: Button = findViewById(R.id.btnDividir)
+        val btnSubtrair: Button = findViewById(R.id.btnSubtrair)
 
         btnSomar.setOnClickListener {
             txtView.setText("${txtFieldValor1.text.toString().toInt() + txtFieldValor2.text.toString().toInt()}")
+        }
+        btnMultiplicar.setOnClickListener {
+            txtView.setText("${txtFieldValor1.text.toString().toInt() * txtFieldValor2.text.toString().toInt()}")
+        }
+
+        btnDividir.setOnClickListener {
+            txtView.setText("${txtFieldValor1.text.toString().toInt() / txtFieldValor2.text.toString().toInt()}")
+        }
+
+        btnSubtrair.setOnClickListener {
+            txtView.setText("${txtFieldValor1.text.toString().toInt() - txtFieldValor2.text.toString().toInt()}")
         }
     }
 }
